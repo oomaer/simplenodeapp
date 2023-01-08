c6dd5335854efb471d4a41be3d347c3e05a08cf4
umair <cyumair@hotmail.com>
2023-01-08 18:58:18 +0500

    selenium test cases

--- test/test.py
+++ test/test.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
+
   
 # instance of Options class allows
 # us to configure Headless Chrome
@@ -23,12 +24,11 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome(options=options)
  
     # Test case method. It should always start with test_
-    def test_empty_input_during_login(self):
-         
+    def test_invalid_input_during_login(self):
         # get driver
         driver = self.driver
-        # get python.org using selenium
-        driver.get("https://web-production-b1d0.up.railway.app/")
+        # get webpage
+        driver.get("https://web-production-b1d0.up.railway.app/login")
  
         # locate element using name
         driver.find_element(By.NAME, "email").send_keys("")
@@ -40,6 +40,90 @@ class PythonOrgSearch(unittest.TestCase):
         #get the text of the error message
         text = driver.find_element(By.CSS_SELECTOR, "#loginMessage").text
         self.assertEqual(text, "Invalid email or password")
+
+
+    def test_invalid_input_during_register(self):
+        # get driver
+        driver = self.driver
+        # get webpage
+        driver.get("https://web-production-b1d0.up.railway.app/register")
+ 
+        # locate element using name
+        driver.find_element(By.NAME, "name").send_keys("")
+        driver.find_element(By.NAME, "email").send_keys("")
+        driver.find_element(By.NAME, "password").send_keys("")
+        #click the button
+        driver.find_element(By.CSS_SELECTOR, "button").click()
+        #wait for 4 seconds
+        time.sleep(4)
+        #get the text of the error message
+        text = driver.find_element(By.CSS_SELECTOR, "#registerMessage").text
+        self.assertEqual(text, "User validation failed: name: Path `name` is required., email: Path `email` is required., password: Path `password` is required.")
+
+
+    def test_duplicate_profile_regestration(self):
+        # get driver
+        driver = self.driver
+        # get webpage
+        driver.get("https://web-production-b1d0.up.railway.app/register")
+ 
+        # locate element using name
+        driver.find_element(By.NAME, "name").send_keys("someperson")
+        driver.find_element(By.NAME, "email").send_keys("myemail1@gmail.com")
+        driver.find_element(By.NAME, "password").send_keys("12345678")
+        #click the button
+        driver.find_element(By.CSS_SELECTOR, "button").click()
+        #wait for 4 seconds
+        time.sleep(4)
+        #get the text of the error message
+        text = driver.find_element(By.CSS_SELECTOR, "#registerMessage").text
+        self.assertEqual(text, "User already exists. Go to Login Page to Log into the account")
+
+
+    def test_go_to_login_page(self):
+        # get driver
+        driver = self.driver
+        # get webpage
+        driver.get("https://web-production-b1d0.up.railway.app/register")
+ 
+        #click the button
+        driver.find_element(By.CSS_SELECTOR, "a").click()
+        #wait for 4 seconds
+        time.sleep(4)
+
+        text = driver.find_element(By.CSS_SELECTOR, "h1").text
+        self.assertEqual(text, "Sign in")
+
+
+    def test_go_to_register_page(self):
+        # get driver
+        driver = self.driver
+        # get webpage
+        driver.get("https://web-production-b1d0.up.railway.app/login")
+ 
+        #click the button
+        driver.find_element(By.CSS_SELECTOR, "a").click()
+        #wait for 4 seconds
+        time.sleep(4)
+      
+        text = driver.find_element(By.CSS_SELECTOR, "h1").text
+        self.assertEqual(text, "Sign Up")
+
+
+    def test_login_with_credentials(self):
+        # get driver
+        driver = self.driver
+        # get webpage
+        driver.get("https://web-production-b1d0.up.railway.app/login")
+        driver.find_element(By.NAME, "email").send_keys("myemail1@gmail.com")
+        driver.find_element(By.NAME, "password").send_keys("12345678")
+        #click the button
+        driver.find_element(By.CSS_SELECTOR, "button").click()
+        #wait for 4 seconds
+        time.sleep(4)
+       
+        text = driver.find_element(By.CSS_SELECTOR, "#maincontent").text
+        self.assertEqual(text, "WELCOME TO HOMEPAGE")
  
     # cleanup method called after every test performed
     def tearDown(self):

42b45bd09f986a7d5fecc780c4742199a413f155
umair <cyumair@hotmail.com>
2023-01-08 13:19:27 +0500

    testing

--- test/test.js
+++ test/test.js
@@ -1,23 +1,24 @@
 
-const webdriver = require('selenium-webdriver');
+// const webdriver = require('selenium-webdriver');
 const assert = require('assert');
-const { Options } = require('selenium-webdriver/chrome');
+// const { Options } = require('selenium-webdriver/chrome');
 
-options = new Options()
-options.headless();
+// options = new Options()
+// options.headless();
 
-const driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
+// const driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
 
 describe('Show Error Message on Empty Input', () => {
     it('should show error message', async () => {
         
-            driver.get('https://web-production-b1d0.up.railway.app/');
-            driver.findElement(webdriver.By.name('email')).sendKeys('');
-            driver.findElement(webdriver.By.css('button')).click();
+            // driver.get('https://web-production-b1d0.up.railway.app/');
+            // driver.findElement(webdriver.By.name('email')).sendKeys('');
+            // driver.findElement(webdriver.By.css('button')).click();
             
             //wait for 5 seconds if error message is not shown
-            await driver.wait(webdriver.until.elementTextIs(driver.findElement(webdriver.By.id("loginMessage")), "Invalid email or password"), 5000);
-            let text = await driver.findElement(webdriver.By.id('loginMessage')).getText();
+            //await driver.wait(webdriver.until.elementTextIs(driver.findElement(webdriver.By.id("loginMessage")), "Invalid email or password"), 5000);
+            //let text = await driver.findElement(webdriver.By.id('loginMessage')).getText();
+            let text = "hello"           
             assert.equal(text, "Invalid email or password");
             driver.quit();
     });
